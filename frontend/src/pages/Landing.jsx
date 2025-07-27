33a02611c2b84d2f4c5c1becffd6ee89e22e1b36
smithwithatypo <smithwithatypo@gmail.com>
2025-07-27 07:10:51 -0500

    add searchbar to landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,10 +1,11 @@
-import '../components/search'
+import { Navbar } from '../components/navbar/Navbar';
+import { Search } from '../components/search'
 
 export const Landing = () => {
     return ( 
         <>
+        <Navbar />
         <Search />
         </>
-
      );
 }
