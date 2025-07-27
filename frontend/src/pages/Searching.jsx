57f8762ea9ad0c67e419be6c0b4ce7792ad801f9
smithwithatypo <smithwithatypo@gmail.com>
2025-07-27 12:46:54 -0500

    add navbar to searching page

--- frontend/src/pages/Searching.jsx
+++ frontend/src/pages/Searching.jsx
@@ -1,9 +1,11 @@
+import { Navbar } from "../components/navbar/Navbar"
 import { Search } from "../components/search/Search"
 
 export const Searching = () => {
 
     return (
         <> 
+        <Navbar />
         <h1> Searching page </h1>
         <Search />
         </>
