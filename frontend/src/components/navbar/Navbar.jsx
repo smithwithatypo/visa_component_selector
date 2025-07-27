0f94ed1505e561b9f83819903f40b2b485ad9a23
smithwithatypo <smithwithatypo@gmail.com>
2025-07-27 13:50:11 -0500

    refactor navbar code and add dark mode icon

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -16,6 +16,7 @@ import {
     VisaMenuLow,
     VisaNotificationsLow,
     VisaSearchLow,
+    VisaModeDarkLow
 } from '@visa/nova-icons-react';
 import {
     Avatar,
@@ -40,7 +41,7 @@ import {
     UtilityFragment,
     VisaLogo,
 } from '@visa/nova-react';
-import { CSSProperties, useRef, useState } from 'react';
+// import { useRef, useState } from 'react';
 import './Navbar.css'
 import { Link as RouterLink } from 'react-router-dom';
 
@@ -48,9 +49,9 @@ const id = 'navbar';
 
 
 export const Navbar = () => {
-  const [expandSearch, setExpandSearch] = useState(false);
-  const searchInitiallyActivated = useRef(false);
-  const homeURL = 'https://design.visa.com';
+//   const [expandSearch, setExpandSearch] = useState(false);
+//   const searchInitiallyActivated = useRef(false);
+  const homeURL = '/';
   const docsURL = 'https://design.visa.com/components/';
   const searchingURL = '/searching';
 
@@ -59,40 +60,10 @@ export const Navbar = () => {
       <Link skipLink href="#content">
         Skip to content
       </Link>
-      <UtilityFragment vJustifyContent="between">
+      <UtilityFragment vJustifyContent="begin">
         <Nav id={id} orientation="horizontal" tag="header">
-          {!expandSearch ? (
             <>
-              {/* <UtilityFragment vContainerHide="desktop">
-                <DropdownButton
-                  aria-controls={`${id}-mobile-menu`}
-                  aria-expanded={mobileMenuOpen ? 'true' : 'false'}
-                  aria-label="open menu"
-                  aria-describedby={`${id}-mobile-menu-notifications-badge`}
-                  buttonSize="large"
-                  colorScheme="tertiary"
-                  iconButton
-                  id={`${id}-mobile-menu-button`}
-                  onClick={onToggleMobileMenu}
-                >
-                  {mobileMenuOpen ? (
-                    <VisaCloseTiny />
-                  ) : (
-                    <>
-                      <VisaMenuLow />
-                      <Badge
-                        id={`${id}-mobile-menu-notifications-badge`}
-                        aria-label="3 notifications"
-                        badgeVariant="number"
-                        tag="sup"
-                      >
-                        3
-                      </Badge>
-                    </>
-                  )}
-                </DropdownButton>
-              </UtilityFragment> */}
-              <UtilityFragment vFlex vGap={16}>
+              <UtilityFragment vFlex vGap={4}>
                 <Link
                   aria-label="Visa Design Home"
                   href={homeURL}
@@ -104,81 +75,39 @@ export const Navbar = () => {
                   {/* <NavAppName>
                     <Utility
                       vContainerHide="xs"
-                      element={<Typography variant="headline-3">VISA Design</Typography>}
+                      element={<Typography variant="headline-3">Component Search</Typography>}
                     />
                   </NavAppName> */}
                 </Link>
-              </UtilityFragment>
-              <UtilityFragment vFlex vJustifyContent="end" vFlexGrow vMarginLeft="auto" vContainerHide="mobile">
-                <nav aria-label="horizontal navigation bar">
-                  <UtilityFragment vGap={4}>
-                    <Tabs>
-                      <Tab>
-                        <Button
-                          buttonSize="large"
-                          colorScheme="tertiary"
-                          element={<a href={docsURL}>Docs</a>}
-                        />
-                      </Tab>
-                      <Tab>
-                        <Button
-                          buttonSize="large"
-                          colorScheme="tertiary"
-                          element={<RouterLink to={searchingURL}>Search</RouterLink>}
-                        />
-                      </Tab>
-                    </Tabs>
-                  </UtilityFragment>
-                </nav>
-              </UtilityFragment>
-              <Utility vFlex vGap={8} vMarginLeft={8}>
-                <Button
-                  aria-label="search site"
-                  buttonSize="large"
-                  colorScheme="tertiary"
-                  iconButton
-                  onClick={() => { setExpandSearch(true); searchInitiallyActivated.current = true; }}
-
-                >
-                  <VisaSearchLow />
-                </Button>
-                
-              </Utility>
-            </>
-          ) : (
-            <UtilityFragment vFlex>
-              <Surface
-                style={
-                  {
-                    '--v-surface-background': 'var(--palette-default-surface-3)',
-                    '--v-surface-border-radius': 'var(--size-rounded-medium)',
-                    '--v-surface-padding-inline': 'var(--size-scalable-8)',
-                  }
-                }
-              >
-                <InputContainer>
-                  <VisaSearchLow />
-                  <Input
-                    id={`${id}-search-field`}
-                    name={`${id}-search-field`}
-                    required
-                    type="search"
-                    aria-label="Search"
-                    placeholder="Search"
-                  />
-                </InputContainer>
-                <Button
-                  aria-label="close search"
-                  buttonSize="large"
-                  colorScheme="tertiary"
-                  iconButton
-                  onClick={() => setExpandSearch(false)}
-                >
-                  <VisaCloseLow />
-                </Button>
-              </Surface>
             </UtilityFragment>
-          )}
+
+            <UtilityFragment vFlex vGap="normal" vMarginLeft={20}>
+                <nav aria-label="horizontal navigation bar">
+                    <Tabs>
+                    <Tab>
+                        <Button
+                        buttonSize="large"
+                        colorScheme="tertiary"
+                        element={<a href={docsURL}>Docs</a>}
+                        />
+                    </Tab>
+                    <Tab>
+                        <Button
+                        buttonSize="large"
+                        colorScheme="tertiary"
+                        element={<RouterLink to={searchingURL}>Search</RouterLink>}
+                        />
+                    </Tab>
+                    </Tabs>
+                </nav>
+            </UtilityFragment>
+
+
+            <UtilityFragment vFlex vJustifyContent="end" vFlexGrow vMarginLeft="auto">
+                <p> <VisaModeDarkLow /> </p>
+            </UtilityFragment>
+          </> 
+          
         </Nav>
       </UtilityFragment>
       
