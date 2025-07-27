8d68d018c070a63b8e812c49b86055de8408fe88
smithwithatypo <smithwithatypo@gmail.com>
2025-07-27 05:01:48 -0500

    changed nova theming at app-level and added button from nova

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Visa components
+import { Button } from '@visa/nova-react';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,9 +21,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
+// React imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+// Visa imports
+import '@visa/nova-styles/styles.css';
+// import '@visa/nova-styles/themes/visa-light/index.css';
+import '@visa/nova-styles/themes/visa-dark/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
