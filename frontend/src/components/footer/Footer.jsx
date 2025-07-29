03ea9154e90f402cb1df9137b92e03c2eef0690d
smithwithatypo <smithwithatypo@gmail.com>
2025-07-29 09:14:29 -0500

    update footer to link to github

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import { Footer, Utility, Typography } from '@visa/nova-react';
+import { GenericCodeRunTiny } from '@visa/nova-icons-react';
+import './Footer.css'
+
 
 export const DefaultFooter = () => {
   return (
     <Footer className="v-gap-15">
       <Utility vFlex vFlexWrap vFlexGrow vJustifyContent="center">
-        <Typography variant="label-small" colorScheme="subtle">Made by Stephen</Typography>
+        <Typography variant="label-small" colorScheme="subtle">
+          <a href='https://github.com/smithwithatypo/visa_component_selector' className='icon-link'>
+            <GenericCodeRunTiny />
+          </a>
+          Made by Stephen
+        </Typography>
       </Utility>
     </Footer>
   );
