fc65ccf9e2ef963d8c38bd322952c5eb087b3af9
smithwithatypo <smithwithatypo@gmail.com>
2025-07-28 23:12:54 -0500

    add env variables for development and production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3000';
 
+const environment = import.meta.env.MODE || 'development';
+let API_BASE_URL;
+
+if (environment === 'production') {
+  API_BASE_URL = import.meta.env.VITE_API_BASE_URL_PRODUCTION || 'https://visacomponentselector-production.up.railway.app:3000';
+} else {
+  API_BASE_URL = import.meta.env.VITE_API_BASE_URL_DEVELOPMENT || 'http://localhost:3000';
+}
 
 export async function makeGetRequest() {
   try {
